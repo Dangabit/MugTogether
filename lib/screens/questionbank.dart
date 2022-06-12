bc98ea6d40d7dd4a4234e07cce9267008ddacc67
Dangabit <ericgohky@gmail.com>
2022-06-12 22:16:19 +0800

    Implemented Dropdown for module searching

--- lib/screens/questionbank.dart
+++ lib/screens/questionbank.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mug_together/widgets/data.dart';
 import 'package:mug_together/widgets/in_app_drawer.dart';
+import 'package:mug_together/widgets/module_list.dart';
 
 class QuestionBankPage extends StatefulWidget {
   const QuestionBankPage({Key? key}) : super(key: key);
@@ -9,11 +11,29 @@ class QuestionBankPage extends StatefulWidget {
 }
 
 class _QuestionBankPage extends State<QuestionBankPage> {
+  Data currentValue = Data();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Question Bank")),
-      drawer: InAppDrawer.gibDrawer(context),
-    );
+        appBar: AppBar(title: const Text("Question Bank")),
+        drawer: InAppDrawer.gibDrawer(context),
+        body: Column(
+          children: <Widget>[
+            ModuleList.createListing(currentValue),
+            ElevatedButton(
+              onPressed: _submit,
+              child: const Icon(Icons.arrow_right),
+            )
+          ],
+        ));
+  }
+
+  void _submit() {
+    if (currentValue.text != null) {
+      Navigator.pushNamed(context, "/bank/module",
+          arguments: {"mod": currentValue.text});
+    }
+    ;
   }
 }
