ea907866ce7675a672eec5c941a9317e102d6f6a
Dangabit <ericgohky@gmail.com>
2022-06-23 20:25:38 +0800

    Written signup end to end testing

--- integration_test/no_verification_test.dart
+++ integration_test/no_verification_test.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -23,18 +27,20 @@ void main() {
       expect(find.text("Please input your password"), findsOneWidget);
 
       // Test with invalid credentials
-      TextFormField ef = emailField.evaluate().first.widget as TextFormField;
-      ef.controller!.text = "eee";
-      TextFormField pf = passwordField.evaluate().first.widget as TextFormField;
-      pf.controller!.text = "eee";
+      await tester.enterText(emailField, "eee");
+      await tester.ensureVisible(passwordField);
+      await tester.enterText(passwordField, "eee");
+      await tester.ensureVisible(loginButton);
       await tester.tap(loginButton);
       await tester.pumpAndSettle();
       expect(find.text("The email or password is wrong, please try again"),
           findsOneWidget);
 
       // Test with somewhat legit email
+      TextFormField ef = emailField.evaluate().first.widget as TextFormField;
       ef.controller!.clear();
-      ef.controller!.text = "legit@legitly.com";
+      await tester.enterText(emailField, "legit@legitly.com");
+      await tester.ensureVisible(loginButton);
       await tester.tap(loginButton);
       await tester.pumpAndSettle();
       expect(find.text("This email is not used, try creating instead"),
@@ -49,6 +55,81 @@ void main() {
       Finder signupButton = find.byKey(const Key("signupButton"));
       await tester.tap(signupButton);
       await tester.pumpAndSettle();
+
+      // At sign up page, finding all required widgets
+      expect(find.text("Sign Up"), findsOneWidget);
+      Finder uf = find.byKey(const Key("usernameField"));
+      Finder ef = find.byKey(const Key("emailField"));
+      Finder pf = find.byKey(const Key("passwordField"));
+      Finder cpf = find.byKey(const Key("confirmPasswordField"));
+      Finder cb = find.byKey(const Key("createUser"));
+
+      // Test empty fields
+      await tester.tap(cb);
+      await tester.pumpAndSettle();
+      expect(find.text("Please input an NUS email"), findsOneWidget);
+      expect(find.text("Please input your password"), findsOneWidget);
+      expect(find.text("Please input a username"), findsOneWidget);
+      expect(find.text("Please confirm your password"), findsOneWidget);
+
+      // Test password unmatch
+      await tester.enterText(pf, "password1");
+      await tester.ensureVisible(cpf);
+      await tester.enterText(cpf, "password2");
+      await tester.ensureVisible(cb);
+      await tester.tap(cb);
+      await tester.pumpAndSettle();
+      expect(find.text("Password does not match"), findsOneWidget);
+
+      // Test invalid NUS mail
+      await tester.enterText(ef, "legit@legitly.com");
+      await tester.ensureVisible(cb);
+      await tester.tap(cb);
+      await tester.pumpAndSettle();
+      expect(find.text("Please input an NUS email"), findsOneWidget);
+
+      // Enter correct credentials
+      final String resp =
+          await rootBundle.loadString("integration_test/credentials.json");
+      final credentials = jsonDecode(resp);
+      await tester.enterText(uf, "testyboi");
+      await tester.ensureVisible(ef);
+      TextFormField ttfef = ef.evaluate().first.widget as TextFormField;
+      ttfef.controller!.clear();
+      await tester.enterText(ef, credentials["email"]);
+      await tester.ensureVisible(pf);
+      TextFormField ttfpf = pf.evaluate().first.widget as TextFormField;
+      ttfpf.controller!.clear();
+      await tester.enterText(pf, credentials["password"]);
+      await tester.ensureVisible(cpf);
+      TextFormField ttfcpf = cpf.evaluate().first.widget as TextFormField;
+      ttfcpf.controller!.clear();
+      await tester.enterText(cpf, credentials["password"]);
+      await tester.ensureVisible(cb);
+      await tester.tap(cb);
+      await Future.delayed(const Duration(seconds: 3));
+      await tester.pumpAndSettle();
+      expect(find.text("Log In"), findsOneWidget);
+
+      // Test logging without verification
+      TextFormField emailField = find
+          .byKey(const Key("emailFormField"))
+          .evaluate()
+          .first
+          .widget as TextFormField;
+      TextFormField passwordField = find
+          .byKey(const Key("passwordFormField"))
+          .evaluate()
+          .first
+          .widget as TextFormField;
+      Finder lb = find.byKey(const Key("loginButton"));
+      emailField.controller!.text = credentials["email"];
+      passwordField.controller!.text = credentials["password"];
+      await tester.tap(lb);
+      await Future.delayed(const Duration(seconds: 1));
+      await tester.pumpAndSettle();
+      expect(find.text("Please verify your email before logging in"),
+          findsOneWidget);
     });
   });
 }
