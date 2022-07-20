ec6e6553a756a95f9f3754da497ba8290a6d3328
Jun How <e0725343@u.nus.edu>
2022-07-21 03:58:00 +0800

    Implemented share qns screen with editable notes

--- lib/screens/myQns/question_share.dart
+++ lib/screens/myQns/question_share.dart
@@ -1,16 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:mug_together/models/question.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class SharedQuestion extends StatelessWidget {
-  final Future<Question> questionFuture;
+class SharedQuestion extends StatefulWidget {
+  final dynamic data;
   final User user;
 
-  SharedQuestion(
-      {Key? key, required Map<String, dynamic> data, required this.user})
-      : questionFuture =
-            Question.getByPath(data["module"], data["uid"], data["qid"]),
-        super(key: key);
+  const SharedQuestion({Key? key, required this.data, required this.user})
+      : super(key: key);
+
+  @override
+  State<SharedQuestion> createState() => _SharedQuestion();
+}
+
+class _SharedQuestion extends State<SharedQuestion> {
+  late Future<Question> questionFuture;
+  late TextEditingController notesController;
+  late bool _isEnabled;
+  int importance = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    questionFuture = Question.getByPath(
+        widget.data["module"], widget.data["uid"], widget.data["qid"]);
+    notesController = TextEditingController();
+    _isEnabled = false;
+  }
+
+  @override
+  void dispose() {
+    notesController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +42,229 @@ class SharedQuestion extends StatelessWidget {
       future: questionFuture,
       builder: (context, AsyncSnapshot<Question> snapshot) {
         if (snapshot.hasData) {
+          MarkdownBody markdown = MarkdownBody(
+            data: snapshot.data?.data["Notes"],
+            selectable: true,
+            shrinkWrap: true,
+            softLineBreak: true,
+            styleSheet: MarkdownStyleSheet(
+              textScaleFactor: 1.1,
+              blockSpacing: 5,
+            ),
+            onTapLink: (text, url, title) {
+              launchUrlString(url!);
+            },
+          );
+          notesController.text = markdown.data;
           return Scaffold(
-              body: Column(
-            children: [
-              // TODO: Display question info here, use Markdown pacakge too
-              // If needed, u can make a widget to display the question
-              // for both here and view_question
-              Text(snapshot.data?.data["Question"]),
-            ],
-          ));
+              backgroundColor: const Color.fromARGB(255, 242, 233, 248),
+              appBar: AppBar(
+                backgroundColor: Colors.deepPurple,
+                title: const Text("Add Question"),
+                automaticallyImplyLeading: false,
+              ),
+              body: SingleChildScrollView(
+                  child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.question_mark_outlined,
+                        color: Colors.deepPurple,
+                        size: 18.0,
+                      ),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Text(
+                        "Question",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                        vertical: 10.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(),
+                      ),
+                      child: Center(
+                        child: Wrap(
+                          runSpacing: 5.0,
+                          spacing: 5.0,
+                          children: [
+                            Text(
+                              snapshot.data?.data["Question"],
+                              style: const TextStyle(
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.notes_outlined,
+                        color: Colors.deepPurple,
+                        size: 18.0,
+                      ),
+                      const SizedBox(
+                        width: 5.0,
+                      ),
+                      const Text(
+                        "Notes",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Tooltip(
+                        message: !_isEnabled ? "Edit" : "Save",
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              snapshot.data?.data["Notes"] =
+                                  notesController.text;
+                              _isEnabled = !_isEnabled;
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.edit,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                        vertical: 30.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: _isEnabled ? Colors.grey[100] : Colors.grey[300],
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(),
+                      ),
+                      child: _isEnabled
+                          ? TextField(
+                              controller: notesController,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText:
+                                    'Input any notes (Multiline) (Optional)',
+                              ),
+                            )
+                          : markdown,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Module ->",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5.0,
+                      ),
+                      Container(
+                        height: 30.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              snapshot.data?.data["Module"],
+                              style: const TextStyle(
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  SizedBox(
+                    height: 40,
+                    width: 95,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.deepPurple,
+                      ),
+                      onPressed: () {
+                        Question question = Question(<String, dynamic>{
+                          "Question": snapshot.data?.data["Question"],
+                          "Notes": snapshot.data?.data["Notes"],
+                          "Module": snapshot.data?.data["Module"],
+                          "LastUpdate": DateTime.now().toString(),
+                          "Tags": List.empty(),
+                          "Importance": importance,
+                          "Privacy": false,
+                          "Owner": widget.user.uid,
+                          "FromCommunity": true,
+                        }, widget.user.uid, snapshot.data?.data["Module"]);
+                        question.addToDatabase().then((_) =>
+                            Navigator.pushReplacementNamed(
+                                context, "/questions"));
+                      },
+                      child: const Text(
+                        "Add to My Questions",
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )));
         } else {
           return const CircularProgressIndicator();
         }
